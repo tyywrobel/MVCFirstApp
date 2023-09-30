bfe8b45f8e030f4a054c8b9038104946632fac34
tyywrobel <tylerbeitel25@gmail.com>
2023-09-30 19:41:39 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCFirstApp.Models;
+using System.Diagnostics;
+
+namespace MVCFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult cit218()
+        {
+            return View();
+        }
+
+        //GET: Home
+        public string myIndex()
+        {
+            return "Tyler's Awesome Index";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
